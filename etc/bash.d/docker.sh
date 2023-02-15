2529ab9c6228e380d3136fdc475050232454c621
Tim Short <tim@fifthrowtech.com>
2023-02-15 15:15:55 +0000

    backfill

---
+++ etc/bash.d/docker.sh
@@ -0,0 +1,12 @@
+alias dimgs='docker images'
+alias dlogs='docker logs'
+alias   dps='docker ps'
+alias  dpsa='docker ps -a'
+alias  drmi='docker rmi'
+
+alias   vid="vi Dockerfile"
+
+#cat /etc/docker/daemon.json
+#{
+    #"dns": ["8.8.8.8", "1.1.1.1"]
+#}
---
+++ etc/bash.d/hashicorp.sh
@@ -0,0 +1,21 @@
+# --------------------------------
+
+alias      tf="terraform"
+alias tfapply="terraform apply plan"
+alias   tffmt="terraform fmt"
+alias  tfinit="terraform init"
+alias  tfplan="terraform plan -out plan"
+alias   tfval="terraform validate"
+
+alias tfcheck="tffmt && tfval"
+
+alias  lltf="_ls -l *.tf"
+alias lltfx="_ls -l *.tf*"
+
+export TF_PLUGIN_CACHE_DIR=/var/lib/terraform/plugins
+
+alias bdy="boundary"
+export BOUNDARY_ADDR="https://boundary.dev.globedx.io"
+export BOUNDARY_CLI_FORMAT="json"
+
+# --------------------------------
