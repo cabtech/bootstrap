72f7a91576adb943c58e9e13c0169dfe5d8cf4c7
Tim Short <tim.short@cabernet.tech>
2023-03-06 13:51:47 +0000

    chore: add conditional PATH to CabTech scripts

--- etc/bash.d/cabtech.sh
+++ etc/bash.d/cabtech.sh
@@ -1,3 +1,11 @@
 alias qcs='cd ~/src/cabtech'
 alias qcw='cd ~/work/cabtech'
 alias qroles='cd ~/work/cabtech/roles'
+
+if [[ -d ~/src/cabtech/ansible-scripts ]]; then
+	export PATH=${PATH}:~/src/cabtech/ansible-scripts
+fi
+
+if [[ -d ~/src/cabtech/terraform-scripts ]]; then
+	export PATH=${PATH}:~/src/cabtech/terraform-scripts
+fi
