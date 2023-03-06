f9dc32019f6e037c365c06129a8d8c463118ac7c
Tim Short <tim.short@cabernet.tech>
2023-03-06 12:14:56 +0000

    chore: just need shared mount

--- bin/mount-vm-shares.sh
+++ bin/mount-vm-shares.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 dnames='/mnt/hgfs/backups /mnt/hgfs/sandisk /mnt/hgfs/shared'
+dnames='/mnt/hgfs/shared'
 sudo mkdir -p $dnames
 sudo chmod 755 $dnames
 # sudo mount -t fuse.vmhgfs-fuse -o allow_other .host:/backups /mnt/hgfs/backups
-sudo mount -t fuse.vmhgfs-fuse -o allow_other .host:/sandisk /mnt/hgfs/sandisk
+# sudo mount -t fuse.vmhgfs-fuse -o allow_other .host:/sandisk /mnt/hgfs/sandisk
 sudo mount -t fuse.vmhgfs-fuse -o allow_other .host:/shared  /mnt/hgfs/shared
 exit 0
