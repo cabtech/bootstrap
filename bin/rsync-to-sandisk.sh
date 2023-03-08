acf4d3c38fae1acec2c67b1569c443523e009121
Tim Short <tim.short@cabernet.tech>
2023-03-08 21:17:59 +0000

    fix: exclude more

--- bin/rsync-to-sandisk.sh
+++ bin/rsync-to-sandisk.sh
@@ -2,6 +2,6 @@
 sandisk=/mnt/hgfs/sandisk
 target=${sandisk}/backups/$(hostname)
 mkdir -p ${target}
-excludes="--exclude=.local --exclude=.cache --exclude=.docker --exclude=.config/chromium --exclude=venv/ --exclude=venvs/ --exclude=.git"
+excludes="--exclude=.local --exclude=.cache --exclude=.docker --exclude=.config --exclude=venv/ --exclude=venvs/ --exclude=.git"
 rsync -a ${excludes} /home/tim ${target}
 exit 0
