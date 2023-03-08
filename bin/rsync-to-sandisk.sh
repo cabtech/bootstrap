4781f47de55548c39b920aa34ffe9635df291f5c
Tim Short <tim.short@cabernet.tech>
2023-03-08 20:11:59 +0000

    init

---
+++ bin/rsync-to-sandisk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sandisk=/mnt/hgfs/sandisk
+target=${sandisk}/backups/$(hostname)
+mkdir -p ${target}
+excludes="--exclude=.local --exclude=.cache --exclude=.docker --exclude=.config/chromium --exclude=venv/ --exclude=venvs/ --exclude=.git"
+rsync -a ${excludes} /home/tim ${target}
+exit 0
