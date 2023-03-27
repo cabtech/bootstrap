c0cc40bbaebfba3264c56fd77edb69a91dbcd973
Tim Short <tim.short@cabernet.tech>
2023-03-27 19:12:46 +0100

    chore: remove cruft

--- etc/bash.d/ansible.sh
+++ etc/bash.d/ansible.sh
@@ -1,7 +1,5 @@
 # --------------------------------
-alias alint='ansible-lint'
 alias f8='flake8 --ignore=E501'
-alias use-ansible="source ~/work/venv/ansible-2.13/bin/activate"
 alias galaxy="ansible-galaxy install -r requirements.yml -p roles/galaxy"
 alias collections="ansible-galaxy collection list"
 # --------------------------------
