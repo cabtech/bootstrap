0305df90453c0d1e6e574b7f10aa866e5d2a2887
Tim Short <tim.short@cabernet.tech>
2024-02-12 16:57:16 +0000

    lint

--- bin/install-cabtech.sh
+++ bin/install-cabtech.sh
@@ -5,24 +5,36 @@ base=$(cd $(dirname $0)/.. && pwd)
 target=~/src/cabtech
 mkdir -p $target
 cd $target
-git clone https://github.com/cabtech/ansible-scripts.git
-git clone https://github.com/cabtech/bootstrap.git
-git clone https://github.com/cabtech/terraform-scripts.git
+for item in $(grep -v '#' $base/etc/cabtech-repos.cfg); do
+	if [[ ! -d $target/$item ]]; then
+		git clone https://github.com/cabtech/${item}.git
+	else
+		echo "# skipping https clone of $item"
+	fi
+done
 
 target=~/work/cabtech
 mkdir -p $target
 cd $target
-git clone git@github.com:cabtech/ansible-scripts.git
-git clone git@github.com:cabtech/bootstrap.git
-git clone git@github.com:cabtech/terraform-scripts.git
+for item in $(grep -v '#' $base/etc/cabtech-repos.cfg); do
+	if [[ ! -d $target/$item ]]; then
+		git clone git@github.com:cabtech/${item}.git
+	else
+		echo "# skipping git clone $item"
+	fi
+done
 
 target=~/work/cabtech/roles
 mkdir -p $target
 cd $target
-for role in $(cat $base/etc/cabtech-roles.cfg); do
-git clone git@github.com:cabtech/${role}.git
+for item in $(grep -v '#' $base/etc/cabtech-roles.cfg); do
+	if [[ ! -d $target/$item ]]; then
+		git clone git@github.com:cabtech/${item}.git
+	else
+		echo "# skipping git clone $item"
+	fi
 done
 
-~/src/cabtech/ansible-scripts/bin/ct-mkssh.sh -dx
+~/src/cabtech/ansible-scripts/ct-mkssh.sh -x
 
 exit 0
