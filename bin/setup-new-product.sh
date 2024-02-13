d0ffcca223e1cc7857dbe7b853dd07031b441d40
Tim Short <tim.short@cabernet.tech>
2024-02-13 21:25:25 +0000

    feature: install requirements.yml

--- bin/setup-new-product.sh
+++ bin/setup-new-product.sh
@@ -6,19 +6,20 @@ base=$( cd $(dirname $0)/.. && pwd -P)
 
 fname=ansible.cfg
 if [[ ! -e "$fname" ]]; then
-	echo "Installing $fname"
+	echo "# Installing $fname"
 	cp $base/etc/${fname} .
 fi
 
-# --------------------------------
-
-linters="ansible-lint.yml tflint.hcl yamllint"
 if [[ ! -d .config ]]; then
-	mkdir .config
-	for item in $linters; do
-		echo "Installing $item"
-		cp $base/etc/${item} .config
-	done
+	echo "# Installing config for linters"
+	mkdir -p .config
+	rsync -a $base/etc/linters/ .config
+fi
+
+fname=requirements.yml
+if [[ ! -e "$fname" ]]; then
+	echo "# Installing $fname"
+    cp $base/etc/${fname} .
 fi
 
 # --------------------------------
