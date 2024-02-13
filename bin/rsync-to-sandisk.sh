545c5268bcb1ddafc2fd5e8d852e8b7d3e67ad74
Tim Short <tim.short@cabernet.tech>
2024-02-13 11:06:03 +0000

    chore: superseded

--- bin/rsync-to-sandisk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sandisk=/mnt/hgfs/sandisk
-target=${sandisk}/backups/$(hostname)
-mkdir -p ${target}
-excludes="--exclude=.local --exclude=.cache --exclude=.docker --exclude=.config --exclude=venv/ --exclude=venvs/ --exclude=.git"
-rsync -a ${excludes} /home/tim ${target}
-exit 0
