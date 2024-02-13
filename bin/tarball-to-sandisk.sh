a726c43c6aaf2ff8bc1be27a35d69a3995f16a22
Tim Short <tim.short@cabernet.tech>
2024-02-13 09:53:01 +0000

    feature: form tarball locally before pushing to sandisk

--- bin/tarball-to-sandisk.sh
+++ bin/tarball-to-sandisk.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-target=/mnt/hgfs/sandisk/backups/$(hostname)
-mkdir -p $target
+do_rsync=true
+do_sandisk=true
+while getopts RSrs arg; do
+	case $arg in
+		R) do_rsync=false;;
+		S) do_sandisk=false;;
+		r) do_rsync=true;;
+		s) do_sandisk=true;;
+		*) echo "bad arg"; exit 42;;
+	esac
+done
 
-tmpdir=/mnt/hgfs/shared/tmp/$(hostname)
-mkdir -p $tmpdir
-excludes="--exclude=.cache --exclude=.config --exclude=.docker --exclude=.git --exclude=.local --exclude=.mozilla --exclude=venv/ --exclude=venvs/"
-rsync -a ${excludes} /home/tim ${tmpdir}
-
+machine=$(hostname)
+sandisk_base=/mnt/hgfs/sandisk
+sandisk_dir=${sandisk_base}/backups/${machine}
+shared_base=/mnt/hgfs/shared
+shared_dir=${shared_base}/backups/${machine}
 timestamp=$(date +%Y%m%d)
-cd $tmpdir
-tar cf ${target}/${timestamp}.tar .
+rsync_excludes="--exclude=product/disruptor/ --exclude=product/world/ --exclude=product/research/ --exclude=.local --exclude=.cache --exclude=.docker --exclude=.config --exclude=venv/ --exclude=venvs/ --exclude=.git"
+
+mkdir -p $shared_dir
+mkdir -p $sandisk_dir
+
+if $do_rsync; then
+	echo "Running rsync"
+	rsync -a --no-links ${rsync_excludes} /home/tim ${shared_dir}
+fi
+
+if $do_sandisk; then
+	if [[ -d "$shared_dir" ]]; then
+		echo "Creating tarball"
+		cd $shared_dir
+		tmptar=${shared_base}/${machine}.tar
+		tar cf ${tmptar} .
+
+		echo "Moving tarball"
+		/bin/cp -p $tmptar ${sandisk_dir}/${timestamp}.tar
+		/bin/rm -f $tmptar
+	fi
+fi
 
 exit 0
