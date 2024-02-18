8d5c4eeaaa858fb0b32aba7f77f97964a6395a3f
Tim Short <tim.short@cabernet.tech>
2024-02-18 16:46:31 +0000

    feature: add Makefile

--- bin/setup-new-product.sh
+++ bin/setup-new-product.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
 base=$( cd $(dirname $0)/.. && pwd -P)
+ss_verbose=false
+
+while getopts v arg; do
+	case $arg in
+		v) ss_verbose=true;;
+		*) echo "ERROR :: bad arg"; exit 42;;
+	esac
+done
 
 # --------------------------------
 
 fname=ansible.cfg
 if [[ ! -e "$fname" ]]; then
-	echo "# Installing $fname"
-	cp $base/etc/${fname} .
+	ss_verbose && echo "# Installing $fname"
+	/bin/cp $base/etc/${fname} .
 fi
 
 if [[ ! -d .config ]]; then
-	echo "# Installing config for linters"
+	ss_verbose && echo "# Installing config for linters"
 	mkdir -p .config
 	rsync -a $base/etc/linters/ .config
 fi
 
 fname=requirements.yml
 if [[ ! -e "$fname" ]]; then
-	echo "# Installing $fname"
-    cp $base/etc/${fname} .
+	ss_verbose && echo "# Installing $fname"
+	/bin/cp $base/etc/${fname} .
+fi
+
+fname=Makefile
+if [[ ! -e "$fname" ]]; then
+	ss_verbose && echo "# Installing $fname"
+	/bin/cp $base/etc/${fname} .
 fi
 
 # --------------------------------
+
 exit 0
