66045ed1bfc78aeeed34aa48df45b29395e36d20
Tim Short <tim.short@cabernet.tech>
2024-02-19 21:15:54 +0000

    feature: support CIDR for VPC

--- bin/setup-new-product.sh
+++ bin/setup-new-product.sh
@@ -2,15 +2,17 @@
 
 base=$( cd $(dirname $0)/.. && pwd -P)
 ss_acctid=""
+ss_cidr=""
 ss_domain=""
 ss_org=""
 ss_product=""
 ss_user=timshort
 ss_verbose=false
 
-while getopts a:d:o:p:u:v arg; do
+while getopts a:c:d:o:p:u:v arg; do
 	case $arg in
 		a) ss_acctid="${OPTARG}";;
+		c) ss_cidr="${OPTARG}";;
 		d) ss_domain="${OPTARG}";;
 		o) ss_org="${OPTARG}";;
 		p) ss_product="${OPTARG}";;
@@ -87,6 +89,12 @@ if [[ -n "$ss_org" ]]; then
 				$ss_verbose && echo "# Generating $prikey"
 				ssh-keygen -t ed25519 -a 100 -P "" -f $prikey
 				/bin/cp ${prikey}.pub .
+			fi
+
+			fname=aws.yml
+			if [[ ! -e "vars/$fname" ]]; then
+				$ss_verbose && echo "# Rendering vars/$fname"
+				cat $base/template/$fname | sed "s/__CIDR__/${ss_cidr}/" | sed "s/__ORG__/${ss_org}/" | sed "s/__DOMAIN__/${ss_domain}/" | sed "s/__PRODUCT__/${ss_product}/" > vars/$fname
 			fi
 
 			fname=boot.env
