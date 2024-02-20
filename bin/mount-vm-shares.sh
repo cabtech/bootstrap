7c2979f4e75b575b278b49be2ab74f92a97e80ae
Tim Short <tim.short@cabernet.tech>
2024-02-20 16:48:27 +0000

    chore: add df to end of script

--- bin/mount-vm-shares.sh
+++ bin/mount-vm-shares.sh
@@ -3,8 +3,11 @@ base=/mnt/hgfs
 
 shares="sandisk shared"
 for item in $shares; do
+	echo "# $item"
 	sudo mkdir -p $base/$item
 	sudo chmod 755 $base/$item
 	sudo mount -t fuse.vmhgfs-fuse -o allow_other .host:/${item} ${base}/${item}
 done
+
+df -h
 exit 0
