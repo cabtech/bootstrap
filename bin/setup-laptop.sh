2f2be83276cf4e2c2879c133f0ae4ccec3534f96
Tim Short <tim.short@cabernet.tech>
2026-09-25 13:59:28 +0100

    feat: adding more packages

--- bin/setup-laptop.sh
+++ bin/setup-laptop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo apt install curl gzip jq ncal net-tools yamllint
+sudo snap install astral-uv --classic
+sudo snap install ghostty --classic
+sudo snap install ruff
+# install starship
+mkdir -p ~/.config && touch ~/.config/starship.toml
 exit 0
